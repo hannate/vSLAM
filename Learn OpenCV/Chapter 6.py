0c8f409e64a5d169e32223f3e3b41ed9f9bac1ba
hannate <71795519+hannate@users.noreply.github.com>
2021-12-01 21:22:04 -0500

    Implemented simple color detection algorithm

--- Learn OpenCV/Chapter 6.py
+++ Learn OpenCV/Chapter 6.py
@@ -2,9 +2,11 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("Learn OpenCV/Resources/dice.png")
+img = cv2.imread("Learn OpenCV/Resources/mandelbrot.png")
 
+hor = np.hstack((img,img)) # I flex taped these matrices together!
+ver = np.vstack((img,img)) # Images do have to be the same format and size
 
-
-cv2.imshow("Image", img)
+cv2.imshow("Horizontally Stacked", hor)
+cv2.imshow("Vertically Stacked", ver)
 cv2.waitKey(0)
